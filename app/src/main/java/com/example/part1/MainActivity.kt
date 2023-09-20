a72b40fed7c20ea207f2199909c01eece13363a4
Tofriev <t.voelkel@posteo,de>
2023-09-20 12:27:47 +0300

    added App Bar and Add Button

--- app/src/main/java/com/example/part1/MainActivity.kt
+++ app/src/main/java/com/example/part1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.part1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -22,9 +27,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -33,7 +38,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.part1.ui.theme.Part1Theme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,11 +55,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun Calculator() {
     var num1 by remember { mutableStateOf("") }
     var num2 by remember { mutableStateOf("") }
+    var showSum by remember { mutableStateOf(false) }
+
 
     val sum = remember { derivedStateOf {
         val intNum1 = num1.toIntOrNull() ?: 0
@@ -63,57 +69,92 @@ fun Calculator() {
         intNum1 + intNum2
     } }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(30.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
 
-    ){
-        Text(
-            text = "The Summing App",
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.ExtraBold
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Summing App",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = Color.White,
+                ),
+            )
+        },
+        content = {innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                TextField(
+                    value = num1,
+                    onValueChange = { value ->
+                        num1 = value
+                        showSum = false
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    ),
+                    label = { Text("Number 1") },
+                    modifier = Modifier.padding(20.dp),
+                    maxLines = 1
                 )
-            )
 
-        Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    value = num2,
+                    onValueChange = { value ->
+                        num2 = value
+                        showSum = false
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done
+                    ),
+                    label = { Text("Number 2") },
+                    modifier = Modifier.padding(20.dp),
+                    maxLines = 1,
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            showSum = true
+                        }
+                    )
+                )
 
-        TextField(
-            value = num1,
-            onValueChange = { value ->
-                num1 = value
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next),
-            label = { Text("Number 1") },
-            modifier = Modifier.padding(20.dp),
-            maxLines = 1
-        )
+                Button(
+                    onClick = {
+                        showSum = true
+                    },
+                    modifier = Modifier.padding(20.dp)
+                ) {
+                    Text(text = "Add")
+                }
 
-        TextField(
-            value = num2,
-            onValueChange = { value ->
-                num2 = value
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done),
-            label = { Text("Number 2") },
-            modifier = Modifier.padding(20.dp),
-            maxLines = 1
-        )
+                Spacer(modifier = Modifier.height(70.dp))
 
-        Spacer(modifier = Modifier.height(100.dp))
+                if (showSum) {
+                    Text(
+                        text = "${sum.value}",
+                        fontSize = 30.sp
+                    )
+                }
 
-        Text(
-                text = "Sum: ${sum.value}"
-            )
 
-    }
+            }
+        }
+    )
 }
+
 
 
 
